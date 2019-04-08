793d5777376518b77266e8092644e5a8f9c341d5
spoperechnyi <poperechnyi1@gmail.com>
2019-04-08 12:20:43 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+'use stricts';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    // mode: 'development',
+    entry: './home',
+    output: {
+        filename: 'build.js',
+        library: "home"
+    },
+
+    watch: NODE_ENV == 'development',
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV == 'development' ? 'source-map' : 'null',
+
+    plugins: [
+        new webpack.DefinePlugin({NODE_ENV:JSON.stringify(NODE_ENV),
+        LANG: JSON.stringify('ru')})
+    ],
+
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['*','.js']
+    },
+
+    resolveLoader: {
+        modules: ['node_modules'],
+        moduleTemplates: ['*-loader','*'],
+        extensions: ['*','.js']
+    },
+
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader?optional[]=runtime',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }],
+    },
+
+};
+
+if(NODE_ENV == 'production'){
+    module.exports.optimization={
+        minimizer:[new UglifyJsPlugin()]
+    }
+}
