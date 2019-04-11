f60cadbf2a1342a648da38d747a4e840a42e0ff8
spoperechnyi <poperechnyi1@gmail.com>
2019-04-11 11:55:59 +0300

    export_css

--- dist/main.js
+++ dist/main.js
@@ -90,10 +90,22 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("// import \"./index.html\";\r\n\r\nconsole.log(\"Hellow Webpack\");\r\nalert(\"Hellow Webpack\");\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_style_css__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\nconsole.log(\"Hellow Webpack\");\r\nalert(\"Hellow Webpack\");\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/style.css":
+/*!***********************!*\
+  !*** ./src/style.css ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/style.css?");
 
 /***/ })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
@@ -11,6 +12,32 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            // chunkFilename: '[id].css',
+        }),
+    ],
+
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        // options: {
+                        //     // you can specify a publicPath here
+                        //     // by default it uses publicPath in webpackOptions.output
+                        //     publicPath: '../',
+                        //     hmr: process.env.NODE_ENV === 'development',
+                        // },
+                    },
+                    'css-loader',
+                ],
+            },
+        ]
+    }
 }
